6d1af17e2836e8e73e26824210e358b571120205
Kyrill Schmid <kyrillschmid@web.de>
2023-01-03 15:49:02 +0100

    Added new quiz function

--- src/clean-code-quiz/__main__.py
+++ src/clean-code-quiz/__main__.py
@@ -1,28 +1,44 @@
 import streamlit as st
+from components import get_quiz_question
+
+quizzes = [{
+    "q1" : "Elegant and efficient",
+    "q2" : "Written by someone who cares",
+    "q3" : "Well written prose"
+    },
+    {"q1" : "Elegant and efficient",
+    "q2" : "Written by someone who cares",
+    "q3" : "Well written prose"
+    }]
+
 
 if "page" not in st.session_state:
+    current_quiz = 0
     st.session_state.page = 0
 
-def nextpage(): st.session_state.page += 1
-def restart(): st.session_state.page = 0
+def nextpage(): 
+    current_quiz += 1
+    st.session_state.page += 1
+
+def restart(): 
+    current_quiz = 0
+    st.session_state.page = 0
 
 placeholder = st.empty()
-st.button("Next",on_click=nextpage,disabled=(st.session_state.page > 3))
+st.button("Next", on_click=nextpage, disabled=(st.session_state.page > 3))
 
 if st.session_state.page == 0:
     # Replace the placeholder with some text:
-    placeholder.text(f"Hello, this is page {st.session_state.page}")
+    # placeholder.text(f"Hello, this is page {st.session_state.page}")
+    get_quiz_question(placeholder, current_quiz, quizzes[current_quiz])
 
 elif st.session_state.page == 1:
     # Replace the text with a chart:
-    placeholder.line_chart({"data": [1, 5, 2, 6]})
+    get_quiz_question(placeholder, current_quiz, quizzes[current_quiz])
 
 elif st.session_state.page == 2:
 # Replace the chart with several elements:
-    with placeholder.container():
-        st.write("This is one element")
-        st.write("This is another")
-        st.metric("Page:", value=st.session_state.page)
+    get_quiz_question(placeholder, current_quiz, quizzes[current_quiz])
 
 elif st.session_state.page == 3:
     placeholder.markdown(r"$f(x) = \exp{\left(x^🐈\right)}$")
--- src/clean-code-quiz/components.py
+++ src/clean-code-quiz/components.py
@@ -1,19 +1,19 @@
 import streamlit as st
 
 
-def get_quiz_question(key, questions):
+def get_quiz_question(placeholder, key, questions):
 
-    with st.form(f"my_form_{key}"):
+    with placeholder.form(f"my_form_{key}"):
     
-        st.write('**What is Clean Code?**')
+        placeholder.write('**What is Clean Code?**')
         checkbox_vals = []
 
         for key, value in questions.items():
             checkbox_vals.append(st.checkbox(value))
         
         # Every form must have a submit button.
-        submitted = st.form_submit_button("Submit")
+        submitted = placeholder.form_submit_button("Submit")
 
     if submitted:
         for i, answer in enumerate(checkbox_vals):
-            st.write(f"Question: {i} - Answer: {answer}")
+            placeholder.write(f"Question: {i} - Answer: {answer}")
