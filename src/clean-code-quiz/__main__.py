bd9815e662c286dbd9c20155810e1e078c393343
Kyrill Schmid <kyrillschmid@web.de>
2023-01-03 15:07:20 +0100

    Quiz change

--- src/clean-code-quiz/__main__.py
+++ src/clean-code-quiz/__main__.py
@@ -7,6 +7,8 @@ def main():
         
         st.write('**What is Clean Code?**')
 
+    
+
         checkbox_val_1 = st.checkbox("Elegant and efficient")
         checkbox_val_2 = st.checkbox("Written by someone who cares")
         checkbox_val_3 = st.checkbox("Well written prose")
