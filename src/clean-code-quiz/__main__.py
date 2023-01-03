73ff7feb369ab0b5aee4042fd923ba4511d65217
Kyrill Schmid <kyrillschmid@web.de>
2023-01-03 15:29:04 +0100

    Tryed new quiz

--- src/clean-code-quiz/__main__.py
+++ src/clean-code-quiz/__main__.py
@@ -1,30 +1,31 @@
 import streamlit as st
-import pandas as pd
-from components import get_quiz_question
+import numpy as np
+import session_state
+from streamlit.ScriptRunner import RerunException
+from streamlit.ScriptRequestQueue import RerunData
 
-quizzes = [{
-    "q1" : "Elegant and efficient",
-    "q2" : "Written by someone who cares",
-    "q3" : "Well written prose"
-    },
-    {"q1" : "Elegant and efficient",
-    "q2" : "Written by someone who cares",
-    "q3" : "Well written prose"
-    }]
+state = session_state.get(question_number=0)
 
+@st.cache
+def get_question(question_number):
+    arr = np.random.randint(0, 100, 2)
+    q = f"{arr[0]} * {arr[1]}"
+    ans = arr[0]*arr[1]
+    choices = ["Please select an answer", ans, ans-1, ans+1, ans+2]
+    return arr, q, ans, choices
 
+arr, q, ans, choices = get_question(state.question_number)
 
+st.text(f"Solve: {q}")
+a = st.selectbox('Answer:', choices)
 
-def main():
-
-    current_quiz = 0
-    
-    get_quiz_question(current_quiz, quizzes[current_quiz])
-
-    if st.button('Next'):
-        current_quiz += 1
-        get_quiz_question(current_quiz, quizzes[current_quiz])
-    
-
-if __name__ == "__main__":
-    main()
+if a != "Please select an answer":
+    st.write(f"You chose {a}")
+    if (ans == a):
+        st.write("Correct!")
+    else:
+        st.write(f"Wrong!, the correct answer is {ans}")
+            
+if st.button('Next question'):
+    state.question_number += 1
+    raise RerunException(RerunData(widget_state=None))
---
+++ src/clean-code-quiz/old__main__.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import pandas as pd
+from components import get_quiz_question
+
+quizzes = [{
+    "q1" : "Elegant and efficient",
+    "q2" : "Written by someone who cares",
+    "q3" : "Well written prose"
+    },
+    {"q1" : "Elegant and efficient",
+    "q2" : "Written by someone who cares",
+    "q3" : "Well written prose"
+    }]
+
+
+
+
+def main():
+
+    current_quiz = 0
+    
+    get_quiz_question(current_quiz, quizzes[current_quiz])
+
+    if st.button('Next'):
+        current_quiz += 1
+        get_quiz_question(current_quiz, quizzes[current_quiz])
+    
+
+if __name__ == "__main__":
+    main()
