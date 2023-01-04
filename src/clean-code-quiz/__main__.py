c6fabe954be6e5d32b95aebfa27ed56d4d31eb57
Kyrill Schmid <kyrillschmid@web.de>
2023-01-04 12:00:23 +0100

    Quiz v0.9 ready

--- src/clean-code-quiz/__main__.py
+++ src/clean-code-quiz/__main__.py
@@ -1,29 +1,51 @@
 import streamlit as st
-from components import get_quiz_question
+import json
+from components import get_quiz_form
 
-quizzes = [{"What is Clean Code?" : 
-                ["Elegant and efficient", "Written by someone who cares", "Well written prose"]},
-           {"What else is Clean Code?" : 
-                ["Elegant and efficient", "Written by someone who cares", "Well written prose"]},
-           {"What ELSE is Clean Code?" : 
-                ["Elegant and efficient", "Written by someone who cares", "Well written prose"]}
-            ]
 
+with open('style_default.css') as f:
+            st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+if "quiz" not in st.session_state:
+    with open('quizzes/m1_clean_code.json') as json_file:
+        st.session_state.quiz = json.load(json_file)["quiz"]
+    # st.write(st.session_state.quiz)
 
 if "page" not in st.session_state:
     st.session_state.page = 0
 
+if "progress" not in st.session_state:
+    st.session_state["progress"] = 0
+
+
+def previous_page(): 
+    st.session_state.page -= 1
+    st.session_state["progress"] -= 1
+
 def nextpage(): 
     st.session_state.page += 1
+    st.session_state["progress"] += 1
 
 def restart(): 
-    st.session_state.page = 0
+    st.session_state.page = 0   
+    st.session_state["progress"] = 0
 
 placeholder = st.empty()
 
-question = list(quizzes[st.session_state.page].keys())[0]
-answers = list(quizzes[st.session_state.page].values())[0]
+if st.session_state.page < len(st.session_state.quiz):
+    quiz = st.session_state.quiz[st.session_state.page]
+    # st.write(quiz)
+    get_quiz_form(quiz)
+else:
+    emoji = ":rocket:"
+    st.markdown("<h2 style='text-align: center;'>Well done - Quiz Over</h2>", unsafe_allow_html=True)
+    # st.write(f"Well done - Quiz Over!  {emoji}")
+    
 
-get_quiz_question(question, answers)
+col1, col2 = st.columns(2)
+col1.button("Back", on_click=previous_page, disabled=(st.session_state.page == 0))
+col2.button("Next", on_click=nextpage, disabled=(st.session_state.page > len(st.session_state.quiz) - 1))
 
-st.button("Next", on_click=nextpage, disabled=(st.session_state.page > len(quizzes) - 2))
+
+my_bar = st.progress(0)
+my_bar.progress(st.session_state["progress"] / len(st.session_state.quiz))
--- src/clean-code-quiz/components.py
+++ src/clean-code-quiz/components.py
@@ -1,19 +1,37 @@
 import streamlit as st
+from functools import reduce
 
+def get_quiz_form(quiz):
 
-def get_quiz_question(question, answers):
-
+    question = quiz["question"]
+    correct_answers = quiz["answers"]
     with st.form(f"my_form_{question}"):
     
-        st.write(f'**{question}**')
-        checkbox_vals = []
+        if "code" in quiz.keys():
+            code = quiz["code"]
+            code = f'{code}'
+            st.code(code, language='java')
 
-        for answer in answers:
-            checkbox_vals.append(st.checkbox(answer))
+        answers = []
+        options = quiz["options"]
+        st.write(f"**{question}**")
+        for option in options:
+            answers.append(st.checkbox(option))
         
         # Every form must have a submit button.
-        submitted = st.form_submit_button("Submit")
+        submitted = st.form_submit_button("Check answers")
+
+        # st.write(correct_answers)
+        # st.write(question)
+        # st.write(answers)
 
     if submitted:
-        for i, answer in enumerate(checkbox_vals):
-            st.write(f"Question: {i} - Answer: {answer}")
+        all_answers_correct = reduce(lambda x, y: x and y, map(lambda k, l : k == l, answers, correct_answers), True)
+        if all_answers_correct:
+            with open('style_correct.css') as f:
+                st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+        else:
+            with open('style_wrong.css') as f:
+                st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+        
+        
