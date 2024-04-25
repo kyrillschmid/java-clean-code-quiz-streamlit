c516556aac43176c80669647b006e75a19ad49dc
Kyrill Schmid <kyrillschmid@web.de>
2024-04-25 19:06:38 +0200

    new

--- src/clean-code-quiz/__main__.py
+++ src/clean-code-quiz/__main__.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import json
 from components import get_quiz_form
-from leaderboard import save_score, show_leaderboard, reset_leaderboard
+from leaderboard import save_score, reset_leaderboard, show_plot
 
 with open("style_default.css") as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
@@ -12,15 +12,17 @@ if "quiz" not in st.session_state:
         data = json.load(json_file)
         st.session_state.quiz = data["quiz"]
         st.session_state.references = data["references"]
+        st.session_state.info = data["info"]
 
 
 if "page" not in st.session_state:
-    st.session_state.page = 0
+    st.session_state.page = -1
 
 if "progress" not in st.session_state:
     st.session_state["progress"] = 0
 
 if "score" not in st.session_state:
+    st.session_state["score_saved"] = False
     st.session_state["score"] = 0
 
 def previous_page():
@@ -44,26 +46,57 @@ with st.sidebar:
             reset_leaderboard()
             st.success("Leaderboard has been reset.")
 
-if st.session_state.page < len(st.session_state.quiz):
-    quiz = st.session_state.quiz[st.session_state.page]
-    get_quiz_form(quiz)
-else:
+if st.session_state.page == -1:
+    st.subheader(f"Welcome to this week's quiz about {st.session_state.info['title']}")
+    st.markdown(f"{st.session_state.info['description']}")
+    
+    # Using columns to center buttons and show highscore
+    col1, col2, col3 = st.columns([1, 1, 1])
+    with col2:
+        # align the button center
+        if st.button("Start Quiz"): 
+            st.session_state.page = 0
+            st.experimental_rerun()
+    show_plot()
+
+
+elif st.session_state.page >= 0 and st.session_state.page < len(st.session_state.quiz):
+        
+        st.subheader(f"Question {st.session_state.page + 1} of {len(st.session_state.quiz)}")
+
+        quiz = st.session_state.quiz[st.session_state.page]
+        get_quiz_form(quiz)
+
+        col1, col2 = st.columns(2)
+        col1.button("Back", on_click=previous_page, disabled=(st.session_state.page == 0))
+        col2.button("Next", on_click=nextpage, disabled=(st.session_state.page >= len(st.session_state.quiz)))
+
+        my_bar = st.progress(0)
+        my_bar.progress(st.session_state["progress"] / len(st.session_state.quiz))
+
+elif st.session_state.page == len(st.session_state.quiz):
     st.markdown("<h2 style='text-align: center;'>Well done - Quiz Over</h2>", unsafe_allow_html=True)
     name = st.text_input("Enter your name to save your score:")
-    if st.button("Submit Score"):
-        score = st.session_state["score"]
-        save_score(name, score)
+    department = st.selectbox("Enter your department:", ["Accounting", "CMP", "CSS", "CST", "CRD", "ESC", "Events", "GMS", "Legal", "OM", "Marketing", "TA", "TI", "ACE", "CNS", "CEA", "CybSec", "Data & AI", "DevOps", "DiE", "ICE", "IoT", "IT Stabilization", "Mobile", "Ojemba", "PED", "PQE", "Smart Devices", "Venturing", "Web"])
+    col1, col2, col3 = st.columns([1, 1, 1])
+    saved = False
+    with col2:
+        saved = st.button("Submit Score")
+        
+        if saved:
+            score = st.session_state["score"]
+            if st.session_state["score_saved"] == False:
+                save_score(name, department, score)
+                st.session_state["score_saved"] = True
+
+    if saved:        
         st.success("Score saved!")
-        show_leaderboard()
+        show_plot()
         st.balloons()
     
+    # empty space
+    st.markdown("<br>", unsafe_allow_html=True)
     st.markdown("### Additional Resources")
     for ref in st.session_state.references:
         st.markdown(f"[{ref['description']}]({ref['url']})", unsafe_allow_html=True)
 
-col1, col2 = st.columns(2)
-col1.button("Back", on_click=previous_page, disabled=(st.session_state.page == 0))
-col2.button("Next", on_click=nextpage, disabled=(st.session_state.page >= len(st.session_state.quiz)))
-
-my_bar = st.progress(0)
-my_bar.progress(st.session_state["progress"] / len(st.session_state.quiz))
--- src/clean-code-quiz/leaderboard.py
+++ src/clean-code-quiz/leaderboard.py
@@ -2,17 +2,18 @@ import pandas as pd
 import streamlit as st
 import json
 
+
 # Read and write to CSV for leaderboard
-def save_score(name, score):
+def save_score(name, department, score):
     try:
         # Load existing scores
         df = pd.read_csv('leaderboard.csv')
     except FileNotFoundError:
         # Initialize empty DataFrame if file doesn't exist
-        df = pd.DataFrame(columns=['Name', 'Score'])
+        df = pd.DataFrame(columns=['Name', "Department", 'Score'])
     
     # Create a new DataFrame for the new entry
-    new_entry = pd.DataFrame({'Name': [name], 'Score': [score]})
+    new_entry = pd.DataFrame({'Name': [name], 'Department': [department], 'Score': [score]})
     
     # Concatenate the old DataFrame with the new entry
     df = pd.concat([df, new_entry], ignore_index=True)
@@ -20,13 +21,44 @@ def save_score(name, score):
     # Save back to CSV
     df.to_csv('leaderboard.csv', index=False)
 
-def show_leaderboard():
+
+def show_plot():
     try:
+        # Load the CSV file
         df = pd.read_csv('leaderboard.csv')
-        df = df.sort_values(by='Score', ascending=False)  # Sort by score descending
-        st.table(df)
+
+        # Aggregate scores by department
+        # Calculate total scores and count of participants per department
+        department_data = df.groupby('Department')['Score'].agg(['sum', 'count'])
+        
+        # Calculate the average score per department
+        department_data['Average Score'] = department_data['sum'] / department_data['count']
+        
+        # Sort the average scores in descending order
+        department_data = department_data.sort_values(by='Average Score', ascending=False)
+
+        # Sort the DataFrame by 'Score' in descending order for leaderboard
+        df = df.sort_values(by='Score', ascending=False)
+
+        # Reset the index to prevent pandas from adding an unwanted index column in Streamlit table
+        df.reset_index(drop=True, inplace=True)
+
+        st.subheader("Normalized Scores by Department")
+            # Prepare chart data
+        chart_data = department_data[['Average Score']].reset_index()
+        chart_data.columns = ['Department', 'Average Score']
+        st.bar_chart(chart_data.set_index('Department'))
+
+        # Display the DataFrame and the plot in Streamlit
+        col1, col2, col3 = st.columns([1, 4, 1])
+        with col2:
+            st.subheader("Leaderboard")
+            st.markdown(df.style.hide(axis='index').to_html(), unsafe_allow_html=True)
+            st.markdown("<br>", unsafe_allow_html=True)  # To add spacing if needed
+
+        
     except FileNotFoundError:
-        st.write("No scores available yet.")
+        st.error("No scores available yet.")
 
 
 def reset_leaderboard():
@@ -35,7 +67,7 @@ def reset_leaderboard():
     if os.path.exists('leaderboard.csv'):
         os.remove('leaderboard.csv')  # Remove the CSV file
         # Optionally, recreate the file with headers if you don't want to check file existence when saving scores
-        df = pd.DataFrame(columns=['Name', 'Score'])
+        df = pd.DataFrame(columns=['Name', "Department", 'Score'])
         df.to_csv('leaderboard.csv', index=False)
         return "Leaderboard reset successfully."
     else:
